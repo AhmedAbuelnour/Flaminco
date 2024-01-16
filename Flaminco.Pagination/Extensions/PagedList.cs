328394b89482217ed9a0f244c78a9cb614d1d231
Ahmed Abuelnour <ahmed.abuelnour@cmeoffshore.com>
2024-01-16 19:01:10 +0200

    pagination

---
+++ Flaminco.Pagination/Extensions/PagedList.cs
@@ -0,0 +1,22 @@
+﻿using Flaminco.Pagination.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Flaminco.Pagination.Implementations
+{
+    public static class PagedListExtensions
+    {
+        public static async Task<PagedList<TItem>> CreateAsync<TItem>(this IQueryable<TItem> query, int page, int pageSize, CancellationToken cancellationToken = default)
+        {
+            return new PagedList<TItem>
+            {
+                Page = page,
+                Items = await query.Skip((page - 1) * pageSize).Take(pageSize).ToListAsync(cancellationToken),
+                TotalCount = await query.CountAsync(cancellationToken),
+                PageSize = pageSize,
+            };
+        }
+    }
+}
---
+++ Flaminco.Pagination/Models/PagedList.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Flaminco.Pagination.Models
+{
+    public class PagedList<T>
+    {
+        public IEnumerable<T> Items { get; set; }
+        public int Page { get; set; }
+        public int PageSize { get; set; }
+        public int TotalCount { get; set; }
+        public bool HasNextPage { get; }
+        public bool HasPreviousPage { get; }
+    }
+}
